5fe0063bb221d88865829c211d7fb85e5367a588
Selvin Toledo <Selvin3901@gmail.com>
2022-12-06 20:05:42 -0800

    add ventana popup y acomodo homepage

---
+++ GTI-telerik/GTI-telerik/addempleado.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="addempleado.aspx.cs" Inherits="GTI_telerik.addempleado" Theme="Tema1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="papapa">
+            
+            <div id="Cuadropop">
+
+            <div id="barra">
+                <asp:Button runat="server" ID="min" Text="-"/>
+                <asp:Button runat="server" ID="max" Text="O"/>
+                <asp:Button runat="server" ID="del" Text="X"/>
+            </div>
+            <div id="encabezado">
+                <h1>Agregar empleado</h1>
+            </div>
+
+            <div id="registro">
+                <h3>Ingrese nombre </h3>
+                <asp:TextBox runat="server" ID="txtnombre"></asp:TextBox>
+
+                <h3>Ingrese apellido </h3>
+                <asp:TextBox runat="server" ID="txtape"></asp:TextBox>
+
+                <h3>Ingrese campo </h3>
+                <asp:TextBox runat="server" ID="TextBox1"></asp:TextBox>
+
+                <h3>Ingrese campo </h3>
+                <asp:TextBox runat="server" ID="TextBox2"></asp:TextBox>
+            </div>
+
+            <div id="boton">
+                <asp:Button runat="server" ID="button" Text="Agregar"/>
+            </div>
+          </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GTI-telerik/GTI-telerik/addempleado.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace GTI_telerik
+{
+
+
+    public partial class addempleado
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control min.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button min;
+
+        /// <summary>
+        /// Control max.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button max;
+
+        /// <summary>
+        /// Control del.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button del;
+
+        /// <summary>
+        /// Control txtnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombre;
+
+        /// <summary>
+        /// Control txtape.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtape;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control button.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button button;
+    }
+}
